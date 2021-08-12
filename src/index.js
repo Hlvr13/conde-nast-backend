f654099a9548edfa197317c5100a91db1d9a6e87
Levi Vazquez <hlvr.13@gmail.com>
2021-08-12 01:40:01 -0700

    🎨 General cleanse

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
+/** Dev can add any initial configuration
+ * Since everything is set within /src, I added an 'initialConfig.js'
+ * just for dotenv config. Developers can add whatever they want over here
+ */
+import './initialConfig';
+/** Server */
 import express from 'express';
-import bodyParser from 'body-parser';
 import cors from 'cors';
 
 require('dotenv').config();
 
+const PORT = process.env.PORT || 5000;
 const app = express();
-const PORT = process.env.PORT || 4000;
-
 app.use(cors());
-app.use(bodyParser.json());
 
-app.get('/', (_, res) => {
-    res.status(200).send('Server working!');
+app.get('/', (_, resp) => {
+    resp.status(200).send('Server working correctly');
 });
 
+
 app.listen(PORT, () => {
-    console.log('Server working successfully.');
+    console.log(`Server working on port ${PORT}`);
 });
