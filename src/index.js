a836775cbce7e26c9f84f5ce9f0149eb3054cc02
Levi Vazquez <hlvr.13@gmail.com>
2021-08-12 01:40:19 -0700

    🔨Add basic graphql implementation

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import './initialConfig';
 import express from 'express';
 import cors from 'cors';
 
-require('dotenv').config();
+/** Graphql */
+import schema from './graphql';
+import { graphqlHTTP } from 'express-graphql';
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -17,6 +19,16 @@ app.get('/', (_, resp) => {
     resp.status(200).send('Server working correctly');
 });
 
+/** Used the most basic configuration since this packag still
+ * uses graphiql in basic config.
+ */
+app.use(
+    '/graphiql',
+    graphqlHTTP({
+        schema,
+        graphiql: process.env.NODE_ENV !== 'production',
+    })
+);
 
 app.listen(PORT, () => {
     console.log(`Server working on port ${PORT}`);
